0937936cd490a87a7ee583571d0ffa0ca2b8e281
Dheeraj chaubey <dheerajubecha@gmail.com>
2025-01-15 22:17:55 +0530

    Enhance About and Navbar components with new features and styling; update assets configuration

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const Navbar = () => {
+
+    const [isScroll, setScroll] = useState(false)
 
     const sideMenuRef = useRef();
 
@@ -14,6 +16,16 @@ const Navbar = () => {
         sideMenuRef.current.style.transform = 'translateX(16rem)'
     }
 
+    useEffect(()=>{
+        window.addEventListener('scroll', ()=>{
+            if(scrollY > 50){
+                setScroll(true)
+            }else{
+                setScroll(false)
+            }
+        })
+    },[])
+
 
 
   return (
@@ -22,12 +34,12 @@ const Navbar = () => {
     <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
         <Image src={assets.header_bg_color} alt='' className='w-full' />
     </div>
-        <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
+        <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : ""}`}>
             <a href="#top">
                 <h1 className='text-4xl font-semibold cursor-pointer mr-14'>Dhiru.</h1>
             </a>
 
-            <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white'>
+            <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : "bg-white shadow-sm bg-opacity-50"} `}>
                 <li><a className='font-Ovo' href="#top">Home</a></li>
                 <li><a className='font-Ovo'  href="#about">About me</a></li>
                 <li><a className='font-Ovo' href="#services">Services</a></li>
