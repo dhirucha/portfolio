b195b7ea488550e8d3596f3035a1767ccfbfd00e
Dheeraj chaubey <dheerajubecha@gmail.com>
2025-01-28 19:36:43 +0530

    Enhance About and Header components with motion effects; improve user experience with animations on text and images

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,19 +1,43 @@
 import { assets, infoList, toolsData } from '@/assets/assets'
 import Image from 'next/image'
-import React from 'react'
+import { motion } from "motion/react"
 
 const About = ({isDarkMode}) => {
   return (
-    <div id='about' className='w-full px-[12%] py-10 scroll-mt-20'>
-        <h4 className='text-center mb-2 text-lg font-Ovo'>Introduction</h4>
-        <h2 className='text-center text-5xl font-Ovo'>About me</h2>
+    <motion.div id='about' className='w-full px-[12%] py-10 scroll-mt-20'
+    initial={{opacity: 0}}
+    whileInView={{opacity: 1}}
+    transition={{duration: 1}}
+    >
+        <motion.h4 
+        initial={{opacity: 0, y: -20}}
+        whileInView={{opacity: 1, y: 0}}
+        transition={{duration: 0.5, delay: 0.5}}
+        className='text-center mb-2 text-lg font-Ovo'>Introduction</motion.h4>
+        <motion.h2
+        initial={{opacity: 0, y: -20}}
+        whileInView={{opacity: 1, y: 0}}
+        transition={{duration: 0.5, delay: 0.5}}
+         className='text-center text-5xl font-Ovo'>About me</motion.h2>
 
-        <div className='flex w-full flex-col lg:flex-row items center gap-20 my-20'>
-          <div className='w-64 sm:w-80 rounded-3xl max-w-none'>
+        <motion.div 
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1}}
+        transition={{duration: 0.8}}
+        className='flex w-full flex-col lg:flex-row items center gap-20 my-20'>
+          <motion.div
+          initial={{opacity: 0, scale: 0.9}}
+          whileInView={{opacity: 1, scale: 1}}
+          transition={{duration: 0.6}} 
+          className='w-64 sm:w-80 rounded-3xl max-w-none'>
             <Image src={assets.user_image} alt='user' className='w-full rounded-3xl'/>
-          </div>
+          </motion.div>
 
-          <div className='flex-1'>
+          <motion.div 
+          initial={{opacity: 0}}
+          whileInView={{opacity: 1}}
+          transition={{duration: 0.6, delay: 0.8}}
+          className='flex-1'>
               <p className='mb-10 max-w-2xl font-Ovo'>
               I'm a dedicated learner with certifications in MongoDB (GeeksforGeeks) and Full-Stack Web Development (Physics Wallah). Currently preparing for TCS Ignite & Smart Hiring 2025, I am focused on enhancing my skills in reasoning, numerical, and verbal abilities, along with coding in C++. 
               </p>
@@ -37,9 +61,9 @@ const About = ({isDarkMode}) => {
                   </li>
                 ))}
               </ul>
-          </div>
-        </div>
-    </div>
+          </motion.div>
+        </motion.div>
+    </motion.div>
   )
 }
 
--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -20,17 +20,37 @@ const Header = () => {
          className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>
                 Hi! I'm Dheeraj Chaubey <Image src={assets.hand_icon} alt='' className='w-6' />
                 </motion.h3>
-        <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>Full Stack Developer based in Mumbai.</h1>  
-        <p className='max-w-2xl mx-auto font-Ovo'>
-        full-stack developer with hands-on experience in modern web technologies. Passionate about building scalable applications and continuously enhancing my problem-solving skills.
-        </p>
-        <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
-            <a href="#contact"
-            className='px-10 py-3 border border-white rounded-full text-white bg-black flex items-center gap-2 dark:bg-transparent'
-            >Contact me  <Image src={assets.right_arrow_white} alt='' className='w-4' /></a>
+        <motion.h1
+        initial={{y: -30, opacity: 0}}
+        whileInView={{y: 0, opacity: 1}}
+        transition={{duration: 0.8, delay: 0.5}}
 
-            <a href="/sample-resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 dark:bg-white dark:text-black'>
-            My Resume<Image src={assets.download_icon} alt='' className='w-4' /></a>
+         className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>Full Stack Developer based in Mumbai.</motion.h1>  
+        <motion.p
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1}}
+        transition={{duration: 0.6, delay: 0.7}}
+
+        className='max-w-2xl mx-auto font-Ovo'>
+        full-stack developer with hands-on experience in modern web technologies. Passionate about building scalable applications and continuously enhancing my problem-solving skills.
+        </motion.p>
+        <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
+            <motion.a 
+            initial={{y: 30, opacity: 0}}
+            whileInView={{y: 0, opacity: 1}}
+            transition={{duration: 0.6, delay: 0.1}}
+
+            href="#contact"
+            className='px-10 py-3 border border-white rounded-full text-white bg-black flex items-center gap-2 dark:bg-transparent'
+            >Contact me  <Image src={assets.right_arrow_white} alt='' className='w-4' /></motion.a>
+
+            <motion.a 
+            initial={{y: 30, opacity: 0}}
+            whileInView={{y: 0, opacity: 1}}
+            transition={{duration: 0.6, delay: 1.2  }}
+
+            href="/sample-resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 dark:bg-white dark:text-black'>
+            My Resume<Image src={assets.download_icon} alt='' className='w-4' /></motion.a>
         </div>      
     </div>
   )
